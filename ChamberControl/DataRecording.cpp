4dd7ff80e6a143ca3be8dc4b2d0982825732e280
Mateusz Janis <mateusz.janis.04@gmail.com>
2026-09-15 11:02:41 +0200

    Further data recording functions

--- ChamberControl/DataRecording.cpp
+++ ChamberControl/DataRecording.cpp
@@ -1,10 +1,12 @@
 #include "DataRecording.h"
 
+unsigned int recoeds_to_write = 0;
+
 void initializeFile(){
     
     std::ofstream file(data_file_path);
 
-        // Zapisanie danych oddzielonych przecinkami i znakiem nowej linii
+    // Zapisanie danych oddzielonych przecinkami i znakiem nowej linii
     file << "DATE" << ","
          << "TIME" << ","
          << "temp_mean" << ","
@@ -37,6 +39,29 @@ bool saveLocally(){
 
 }
 
-void sendToServer(){
+bool initializeServerSSH(){
+
+}
+
+bool sendToServer(){
     
+}
+
+void runDataRecording(){
+
+    unsigned int seconds_to_decrease = 0;
+
+    while (!saveLocally() && seconds_to_decrease < sensors_update_interval) {
+        std::cout << "Failed to save locally. Trying again...": << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        seconds_to_decrease += 10;
+    }
+
+    if (!sendToServer()) {
+        records_to_write++;
+        std::cout << "Failed to save on server. Trying in next cycle...": << std::endl;
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(sensors_update_interval - seconds_to_decrease));
+
 }
